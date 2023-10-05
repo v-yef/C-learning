1240ec197d7b1b75ee207ff4c2eef630ce0e7398
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2023-10-05 19:28:02 +0300

    Added S.Prata-C_Primer_Plus-ch.5-ex.1

---
+++ S.Prata-C_Primer_Plus-ch.5-ex.1/source/main.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : S.Prata-C_Primer_Plus-ch.5-ex.1
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Write a program that converts time in minutes to time in hours
+               and minutes. Use #define or const to create a symbolic constant
+               for 60. Use a while loop to allow the user to enter values
+               repeatedly and terminate the loop if a value for the time of 0
+               or less is entered.
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    const int minutes_to_hour = 60;
+    int input_minutes, minutes, hours;
+
+    printf("Enter number of minutes (0<= to exit)\n");
+    scanf("%d", &input_minutes);
+
+    while (input_minutes > 0)
+    {
+        hours = input_minutes / minutes_to_hour;
+        minutes = input_minutes % minutes_to_hour;
+
+        printf("%d minutes equal to: %d hour(s) %d minute(s)\n\n", input_minutes, hours, minutes);
+
+        printf("Enter number of minutes (0<= to exit)\n");
+        scanf("%d", &input_minutes);
+    }
+
+    printf("\n");
+
+    return 0;
+}
