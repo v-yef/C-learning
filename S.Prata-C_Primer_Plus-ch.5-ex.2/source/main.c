863edcae106810d1c8f357a5af4ac6b967608591
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2023-10-05 19:35:36 +0300

    Added S.Prata-C_Primer_Plus-ch.5-ex.2

---
+++ S.Prata-C_Primer_Plus-ch.5-ex.2/source/main.c
@@ -0,0 +1,37 @@
+/*
+ ============================================================================
+ Name        : S.Prata-C_Primer_Plus-ch.5-ex.2
+ Author      : Viacheslav Yefisko
+ Version     : 0
+ Copyright   : MIT License
+ Description : Write a program that asks for an integer and then prints all
+               the integers from (and including) that value up to (and including)
+               a value larger by 10. (That is, if the input is 5, the output
+               runs from 5 to 15.) Be sure to separate each output value by a
+               space or tab or newline.
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int number, max_number;
+
+    printf("Enter int number\n");
+    scanf("%d", &number);
+
+    max_number = number + 10;
+
+    printf("\n");
+
+    while (number <= max_number)
+    {
+        printf("%d\n", number);
+        number++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
