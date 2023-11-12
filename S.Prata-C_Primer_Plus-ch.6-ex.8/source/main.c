c7ed3b679509728e32ad5979c4464ce998dabcfe
Viacheslav Yefisko <vyach.oleg.88@gmail.com>
2023-11-12 19:41:48 +0200

    Style changes

--- S.Prata-C_Primer_Plus-ch.6-ex.8/source/main.c
+++ S.Prata-C_Primer_Plus-ch.6-ex.8/source/main.c
@@ -18,19 +18,19 @@ int main(void)
 {
     const int NUMS_TO_READ = 2;
 
-    float first_num, second_num;
+    float num_1, num_2;
     int nums_read;
 
     printf("Enter a pair of floating-point numbers (q to quit): ");
-    nums_read = scanf("%f%f", &first_num, &second_num);
+    nums_read = scanf("%f%f", &num_1, &num_2);
 
     while (NUMS_TO_READ == nums_read)
     {
         printf("The difference between %.3f and %.3f divided by their product is: %.3f\n\n",
-               first_num, second_num, fabsf(first_num - second_num) / fabsf(first_num * second_num));
+               num_1, num_2, fabsf(num_1 - num_2) / fabsf(num_1 * num_2));
 
         printf("Enter next pair of floating-point numbers (q to quit): ");
-        nums_read = scanf("%f%f", &first_num, &second_num);
+        nums_read = scanf("%f%f", &num_1, &num_2);
     }
 
     printf("\n=====> DONE <=====\n");
